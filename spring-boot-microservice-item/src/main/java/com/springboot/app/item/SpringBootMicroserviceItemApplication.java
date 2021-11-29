90bd472a8646a713a07eea3734ee40cab605a4c0
pinfantes23 <pinfantes1396@gmail.com>
2021-11-29 17:04:50 +0100

    Escalar microservicos con puerto dinámico

--- spring-boot-microservice-item/src/main/java/com/springboot/app/item/SpringBootMicroserviceItemApplication.java
+++ spring-boot-microservice-item/src/main/java/com/springboot/app/item/SpringBootMicroserviceItemApplication.java
@@ -2,11 +2,11 @@ package com.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
-@RibbonClient(name="servicios-productos")
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class SpringBootMicroserviceItemApplication {
